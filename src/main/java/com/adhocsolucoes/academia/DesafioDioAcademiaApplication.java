41087706a8e1040abee9936e2ae9e8dd611c2066
Alexsandro Oliveira <alexoliveira1975@gmail.com>
2023-04-15 15:30:37 -0300

    Project created

---
+++ src/main/java/com/adhocsolucoes/academia/DesafioDioAcademiaApplication.java
@@ -0,0 +1,13 @@
+package com.adhocsolucoes.academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDioAcademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDioAcademiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adhocsolucoes/academia/DesafioDioAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adhocsolucoes.academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDioAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
